e6f2f6400e0f44ac66d26dd772fd789937453cf7
rodricks <rodricks.premkumar@biogen.com>
2022-09-04 04:52:19 +0530

    filter the table data logic Signed-off-by: rodricks <rodricks.premkumar@biogen.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ my_fruits_list = pandas.read_csv(
     "https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruits_list = my_fruits_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
-streamlit.multiselect("Pick some fruits:", list(
+fruits_to_selected = streamlit.multiselect("Pick some fruits:", list(
     my_fruits_list.index), ['Avocado', 'Strawberries'])
-
+fruits_to_show = my_fruits_list.loc[fruits_to_selected]
 # Display the table on the page.
 streamlit.dataframe(my_fruits_list)
