e390ef576dab465302bc32b0f37190f9c7b6f72e
rodricks <rodricks.premkumar@biogen.com>
2022-09-05 04:57:53 +0530

    Signed-off-by: rodricks <rodricks.premkumar@biogen.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas
+import requests
 streamlit.title("My Parents New Healthy Dinner")
 streamlit.header("Breakfast Menu")
 streamlit.text("🥣 Omega 3 & Blueberry Oatmeal")
@@ -12,6 +13,11 @@ my_fruits_list = my_fruits_list.set_index('Fruit')
 # Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(
     my_fruits_list.index), ['Avocado', 'Strawberries'])
+
 fruits_to_show = my_fruits_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+streamlit.header("Fruityvice Fruit Advice!")
+fruityvice_response = requests.get(
+    "https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruityvice_response.json)
