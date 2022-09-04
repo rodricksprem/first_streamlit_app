893be569b05e3e04a17a3e9dd38313f3f9549311
rodricks <rodricks.premkumar@biogen.com>
2022-09-05 05:05:48 +0530

    Signed-off-by: rodricks <rodricks.premkumar@biogen.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,12 @@ fruits_selected = streamlit.multiselect("Pick some fruits:", list(
 fruits_to_show = my_fruits_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
+fruit_choice = streamlit.text_input(
+    'What fruit would you like information about?', 'Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 streamlit.header("Fruityvice Fruit Advice!")
 fruityvice_response = requests.get(
-    "https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
+    "https://fruityvice.com/api/fruit/"+fruit_choice)
+fruityvice_response_jsonnormalized = pandas.json_normalize(
+    fruityvice_response.json())
+streamlit.text(fruityvice_response_jsonnormalized)
