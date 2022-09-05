a6c62fb8abc3fea0a6fa3567078bb71bd4f010bc
rodricks <rodricks.premkumar@biogen.com>
2022-09-05 09:09:16 +0530

    Signed-off-by: rodricks <rodricks.premkumar@biogen.com>

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,8 @@ def get_fruityvice_data(this_fruit_choice):
 
 def insert_row_snowflake(new_fruit):
     with my_cnx.cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('"+ new_fruit+"'))")
+        my_cur.execute(
+            "insert into fruit_load_list values ('" + new_fruit+"')")
         return ("Thanks for adding "+new_fruit)
 
 
